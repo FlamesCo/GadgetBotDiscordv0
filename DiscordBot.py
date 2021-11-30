d98bd17b079bc2f02fbdaf58fdc5fbea5b5888e7
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-29 19:47:45 -0800

    DiscordBot Nov 29 2021 Monday
    added code

---
+++ DiscordBot.py
@@ -0,0 +1,25 @@
+### this is a test bot that says hello when a user joins the server and greets them
+import nextcord
+import os
+import discord
+import os 
+import sys 
+import asyncio
+import time
+import random
+import json
+import requests
+def main():
+    client = discord.Client()
+    token = os.environ['zlcoKPItptVtVNtektNdOA3Z0En8JlJa']
+    client.run(token)
+
+    def Greeting(message):  # this is the greeting function
+        if message.content.startswith('!hello'):
+            channel = message.channel
+            channel.send('Hello!')
+            ## IF A new user joins the server display their discord tag and greet them
+            if message.author.bot == False:
+                channel.send('Welcome ' + message.author.mention + ' to the server!')
+                channel.send('Please read the rules and guidelines in #rules-and-guidelines')
+                channel.send('If you have any questions, please ask a moderator or admin')
