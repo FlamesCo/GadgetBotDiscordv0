57b9ceb18a8a0142811c4393e0adf3997e339bd8
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-29 20:45:21 -0800

    C Compiler using gcc and cmake
    Compiled on Nov 29 8:45 PM FST

---
+++ Gadgetbot-AUTOC-Compiler.py
@@ -0,0 +1,53 @@
+## hello welcome to autocmake
+## get the version of cmake and gcc
+## check if the version is 3.8.0
+import os 
+import sys 
+print("Welcome to AutoCmake")
+print("This is a simple tool to help you deploy your script to any m1 mac and port windows software to the mac m1 platform.")
+gcc = os.getenv('G')
+cmake = os.getenv('C')
+print("The version you are running is:", gcc, cmake)
+
+## check to see if gcc is installed if not install it
+if os.system('gcc --version') != 0:
+    ## check if your mac is m1 or intel processer
+    if os.system('uname -m') == 0:
+        os.system('brew install gcc')
+    else:
+        os.system('brew cask install gcc')
+        ## check to see if cmake is installed if not install it
+if os.system('cmake --version') != 0:
+    ## check if your mac is m1 or intel processer
+    if os.system('uname -m') == 0:
+        os.system('brew install cmake')
+    else:
+        os.system('brew cask install cmake')
+        print("Check what processer you are running then configure cmake and gcc configs for m1 mac setup to port windows x86 software to the m1 or ios platform.")
+        print("If you are running on a windows x86 machine, you can port your software to the m1 platform by running the following command:")
+        if os.system('uname -m') == 0:
+            print("cmake -G \"Unix Makefiles\"")
+        else:
+            print("cmake -G \"MinGW Makefiles\"")
+        print("If you are running on a windows x64 machine, you can port your software to the m1 platform by running the following command:")
+        if os.system('uname -m') == 0:
+            print("cmake -G \"Unix Makefiles\"")
+        else:
+            print("cmake -G \"MinGW Makefiles\"")
+            ## if you want to compile the program into a dmg file for m1 devices on mac
+            ## you can run the following command
+print("Define the folder of your source code to build")
+print("Example: /Users/$user/Documents/Stuff/Code~/Python 3.0x/GadgetBot-HQRips/DiscordBot.py")
+
+print("Enter the code directory of the repo you want to compile from your local machine")
+folderinput= os.getenv(input("Enter the code directory of the repo you want to compile from your local machine: "))
+print("Syncing with folder: " + folderinput)
+print("Enter the name of the folder you want to compile to")
+cmakeoutput= os.getenv(input("Enter the name of the folder you want to compile to: "))
+print("Syncing with folder: " + cmakeoutput)
+print("Name the app and add it to the applications directory")
+appname= os.getenv(input("Name the app and add it to the applications directory: "))
+print("Syncing with folder: " + appname)
+print("Enter the name of the dmg file you want to create")
+dmgname= os.getenv(input("Enter the name of the dmg file you want to create: "))
+print("This feature is work in progrss AUTOCMake v0.1 ")
